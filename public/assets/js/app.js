115316100386177e2f7ede67903016f821599fd8
Scott Juneau <scott@junlaw.com>
2019-06-17 18:51:16 -0500

    changed the window.onload feature

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,32 +1,30 @@
-window.onload = () => {
-  let devourButton = document.getElementsByClassName("readybtn");
-  let addButton = document.getElementById("addbtn");
+let devourButton = document.getElementsByClassName("readybtn");
+let addButton = document.getElementById("addbtn");
 
-  function devour(event) {
-    let id = this.getAttribute("data-id");
-    console.log(id);
+function devour(event) {
+  let id = this.getAttribute("data-id");
+  console.log(id);
 
-    fetch("/api/burgers/" + id, { method: "PUT" }).then(location.reload());
-  }
+  fetch("/api/burgers/" + id, { method: "PUT" }).then(location.reload());
+}
 
-  function addBurger(event) {
-    event.preventDefault();
-    let data = { burger: document.forms["main"].elements["addburger"].value };
-    console.log(data);
-    fetch("/api/burgers/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }).then(location.reload());
-  }
+function addBurger(event) {
+  event.preventDefault();
+  let data = { burger: document.forms["main"].elements["addburger"].value };
+  console.log(data);
+  fetch("/api/burgers/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }).then(location.reload());
+}
 
-  //Listeners activated
-  for (let i = 0; i < devourButton.length; i++) {
-    devourButton[i].addEventListener("click", devour);
-  }
+//Listeners activated
+for (let i = 0; i < devourButton.length; i++) {
+  devourButton[i].addEventListener("click", devour);
+}
 
-  addButton.addEventListener("click", addBurger);
-};
+addButton.addEventListener("click", addBurger);
